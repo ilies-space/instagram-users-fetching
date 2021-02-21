6aaa02fcc2b451e7902d1c8d8f0f8c0c14cf15ef
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2021-02-21 21:37:04 +0100

    beta

--- App.js
+++ App.js
@@ -1,114 +1,106 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, {useState} from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
   Text,
-  StatusBar,
+  View,
+  FlatList,
+  Image,
+  TextInput,
+  ActivityIndicator,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default function App() {
+  const [query, setQuery] = useState('');
+  const [isLoading, setIsloadin] = useState(false);
+  const [users, setUsers] = useState([]);
 
-const App: () => React$Node = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={{flex: 1, alignItems: 'center', backgroundColor: '#acdbdf'}}>
+      <TextInput
+        style={{
+          borderWidth: 0.5,
+          paddingHorizontal: 50,
+          marginVertical: 5,
+          borderRadius: 20,
+          borderColor: 'gray',
+          alignItems: 'center',
+          justifyContent: 'center',
+          textAlign: 'center',
+          height: 35,
+        }}
+        placeholder={'Search by user name ... '}
+        onChangeText={(value) => {
+          setIsloadin(true);
+
+          setUsers([]);
+          fetch(
+            'https://www.instagram.com/web/search/topsearch/?query=' + value,
+          )
+            .then((response) => response.json())
+            .catch((Error) => alert(Error))
+            .then((data) => {
+              console.log(data.users);
+              setUsers(data.users);
+              setIsloadin(false);
+            });
+        }}
+      />
+      {isLoading ? (
+        <View style={{}}>
+          <ActivityIndicator />
+          <Text>Searching ... </Text>
+        </View>
+      ) : (
+        <View />
+      )}
+
+      <View style={{flex: 1, width: '100%'}}>
+        <FlatList
+          keyExtractor={(item) => {
+            item.pk;
+          }}
+          data={users}
+          renderItem={(item) => {
+            const userInfo = item.item.user;
+            return (
+              <View
+                style={{
+                  backgroundColor: '#f0ece2',
+                  marginBottom: 10,
+                  padding: 10,
+                  margin: 8,
+                  borderRadius: 10,
+                  // alignItems: 'center',
+                }}>
+                <View style={{flexDirection: 'row'}}>
+                  <View style={{marginHorizontal: 15}}>
+                    <Image
+                      style={{
+                        height: 50,
+                        width: 50,
+                        borderRadius: 50 / 2,
+                      }}
+                      source={{
+                        uri: userInfo.profile_pic_url,
+                      }}
+                    />
+                  </View>
+                  <View>
+                    <Text style={{fontWeight: 'bold'}}>
+                      {userInfo.full_name}
+                    </Text>
+                    <Text style={{color: 'grey'}}>{userInfo.username}</Text>
+                    {userInfo.is_private ? (
+                      <Text style={{color: 'red'}}>private</Text>
+                    ) : (
+                      <Text style={{color: 'green'}}>public</Text>
+                    )}
+                  </View>
+                </View>
+              </View>
+            );
+          }}
+        />
+      </View>
+    </View>
   );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+}
